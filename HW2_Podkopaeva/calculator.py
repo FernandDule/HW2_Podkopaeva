676782fd60ae8475a2b98a8b7299b38e119b966a
FernandDule <111347698+FernandDule@users.noreply.github.com>
2026-09-26 20:19:59 +0800

    Add main function and README

--- HW2_Podkopaeva/calculator.py
+++ HW2_Podkopaeva/calculator.py
@@ -9,3 +9,27 @@ def multiplication(a, b):
 
 def division(a, b):
     return a / b
+
+def main():
+    a, operation, b = input().split()
+    
+    if operation == '+':
+        result = addition(float(a), float(b))
+    elif operation == '-':
+        result = subtraction(float(a), float(b))
+    elif operation == '*':
+        result = multiplication(float(a), float(b))
+    elif operation == '/':
+        if float(b) == 0:
+            print('Деление на 0')
+            return
+        result = division(float(a), float(b))
+    else:
+        print('Некорректное выражение')
+        return
+    
+    print(result)
+    return
+
+if __name__ == '__main__':
+    main()
